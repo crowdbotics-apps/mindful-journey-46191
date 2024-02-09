603658b3bfbff73b9d9c3f9253d0dbb2e45206ea
Crowdbotics <team@crowdbotics.com>
2024-02-09 19:47:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "mindful-journey-46191",
+  url: "https://mindful-journey-46191.botics.co",
+  api: "https://mindful-journey-46191.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
